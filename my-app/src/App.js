cc076310e2d6f4de53d41700217cdb5c6158b5fa
MarinaShustrova <averc.marina@yandex.ru>
2023-02-28 13:42:37 +0300

    simple counter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,12 @@
 import React, { useState } from 'react';
+import Counter  from './components/Counter';
 
 function App() {
- const[likes, setLikes] = useState(5);
-
- function increment() {
-setLikes(likes + 1)
- }
-
- function decrement() {
-setLikes(likes - 1)
- }
+ const[value, setValue] = useState('text in input');
   
   return (
     <div className="App">
-    <h1>{likes}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement }>Decrement</button>
+  <Counter  />
     </div>
   );
 }
