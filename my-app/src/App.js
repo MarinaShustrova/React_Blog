28645baf6f99702387417b9a1390f51117d4b561
MarinaShustrova <averc.marina@yandex.ru>
2023-03-03 18:51:52 +0300

    sorted list done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,7 @@ import PostList from './components/PostList';
 // import MyButton from './components/UI/button/MyButton'
 // import MyInput from './components/UI/input/MyInput'
 import PostForm from './components/PostForm';
+import MySelect from './components/UI/select /MySelect';
 
 function App() {
  const  [posts, setPosts ] = useState([
@@ -15,16 +16,35 @@ function App() {
     {id: 3, title: 'JavaScript', body: 'Description'},
   ])
 
+  const [selectedSort, setSelectedSort] = useState('');
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
   }
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
+
+const sortPosts = (sort) => {
+  setSelectedSort(sort);
+  setPosts([...posts].sort((a,b) => a[sort].localCompare(b[sort])))
+}
  
   return (
     <div className="App">
   <PostForm  create={createPost}/>
+  <hr style={{margin: '15 px 0'}}/>
+  <div>
+    <MySelect
+    value={selectedSort}
+    onChange={sortPosts}
+    defaultValue='Sorted By'
+    options={[
+      {value: 'title', name:'By name'},
+      {value: 'body', name:'By description'},
+    ]}
+     />
+  </div>
    {
     posts.length !== 0
     ?  <PostList remove={removePost}  posts={posts} title= 'POST ABOUT JS' />
